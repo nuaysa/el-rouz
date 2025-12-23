35f07afaac5bc082517bdf1aaf55bfd3a4cd54da
Hepytech Dev <hepytech@hallomepy.com>
2025-12-23 12:45:02 +0700

    fix: fix contact number

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,6 +2,6 @@ import Link from "next/link";
 
 export default function Footer() {
     return(
-        <div className="bg-black flex justify-center items-center text-white h-20 ">Developed by <Link href="https://aysa.bim.web.id/">A</Link> © 2025 EL-ROUZ CATERING. Cita Rasa Autentik Timur Tengah.</div>
+        <div className="bg-black flex justify-center items-center text-white h-20 ">Developed by <Link href="https://aysa.bim.web.id/"> A </Link> © 2025 EL-ROUZ CATERING. Cita Rasa Autentik Timur Tengah.</div>
     )
 }
